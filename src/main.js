de449367c5305bed30c4bf9467dbb0ec2c24c8a5
Aries Cs <aries0d0f@gmail.com>
2018-05-21 17:35:26 +0800

    Add fontawesome icon.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue        from 'vue'
 import App        from './App'
 import router     from './router'
 
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+// the library
+import fontawesome from '@fortawesome/fontawesome'
+// add more icon categories as you want them, even works with pro packs
+import brands from '@fortawesome/fontawesome-free-brands'
+import solid from '@fortawesome/fontawesome-free-solid'
+
+// asociate it to the library, if you need to add more you can separate them by a comma
+fontawesome.library.add(brands)
+fontawesome.library.add(solid)
+Vue.component(FontAwesomeIcon.name, FontAwesomeIcon)
+
 Vue.config.devtools = true
 
 const app = new Vue({
@@ -9,4 +21,4 @@ const app = new Vue({
   ...App
 }).$mount('#app')
 
-export default { app, router }
+export default { app }
