5da3b3a8f86cb2062f6cae399a6af5392e9118ef
Aries Cs <aries0d0f@gmail.com>
2018-10-10 22:04:46 +0800

    Layout: modify footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="footer">
     <p>This work is licensed under MIT License</p>
-    <p>Design by Aries Cs</p>
+    <p>Design by <a href="https://aries0d0f.me">Aries Cs</a></p>
   </div>
 </template>
