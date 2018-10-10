ead8bf0d87fa1ffeb0a7c900910640c6e646ebde
Aries Cs <aries0d0f@gmail.com>
2018-10-11 00:26:55 +0800

    Layout: add language switch to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,5 +2,17 @@
   <div id="footer">
     <p>This work is licensed under MIT License</p>
     <p>Design by <a href="https://aries0d0f.me">Aries Cs</a></p>
+    <p v-if="lang.length > 1"> • <span v-for="lang in lang" :key="lang.name"><a :href="`?lang=${lang.name}`">{{ lang.human }}</a> • </span></p>
   </div>
 </template>
+
+<script>
+  import * as profile from '../../profile.json'
+  export default {
+    data () {
+      return {
+        lang: profile.mulitLang
+      }
+    }
+  }
+</script>
