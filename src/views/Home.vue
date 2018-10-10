9670c0f052766531fa17b42e0d8a64d317279fbf
Aries Cs <aries0d0f@gmail.com>
2018-10-10 23:24:51 +0800

    Layout: add mulit lang support

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div id="head" class="card card-head">
       <div class="image" :style="{ 'background-image': `url(${photo})` }"></div>
       <div class="content">
-        <h1>{{ name }}</h1>
+        <h1>{{ name[lang.index] }}</h1>
         <p>{{ id }}</p>
       </div>
     </div>
@@ -17,7 +17,7 @@
     </div>
     <div id="about" class="card">
       <h2>About</h2>
-      <p v-for="paragraph in about" :key="paragraph">{{ paragraph }}</p>
+      <p v-for="paragraph in about[lang.index]" :key="paragraph">{{ paragraph }}</p>
     </div>
     <div id="skill" class="card">
       <h2>Skill</h2>
@@ -30,7 +30,7 @@
     </div>
     <div id="experience" class="card">
       <h2>Experience</h2>
-      <div v-for="(item, i) in experience" :key="i">
+      <div v-for="(item, i) in experience[lang.index]" :key="i">
         <p class="tag">{{ item.date }}</p>
         <p>{{ item.title }}</p>
       </div>
@@ -42,7 +42,19 @@
   import * as profile from '../../profile.json'
   export default {
     data () {
-      return profile
+      return {
+        ...profile,
+        lang: profile.mulitLang[0]
+      }
+    },
+    mounted () {
+      this.langDetect()
+    },
+    methods: {
+      langDetect () {
+        console.log(profile.mulitLang.filter(e => (e.name === this.$route.query.lang))[0])
+        this.lang = this.$route.query.lang ? profile.mulitLang.filter(e => (e.name === this.$route.query.lang))[0] : profile.mulitLang[0]
+      }
     }
   }
 </script>
